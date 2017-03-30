254bbe52608b86462db0498766218756e28d5053
Keola Stone <skstone808@gmail.com>
2017-03-29 20:52:26 -1000

    pau

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 //Secret Menu Challenge
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var addMc = document.getElementsByClassName("name");
+console.log(addMc);
 
+for(var i=0; i<addMc.length; i++){
+  addMc[i].addEventListener("click", displayMc);
+}
+
+function displayMc(){
+  var getMc = this.querySelectorAll(".menu")[0];
+  if(getMc.style.display === "none"){
+    getMc.style.display = "block";
+  }else{
+    getMc.style.display = "none";
+  }
+}
